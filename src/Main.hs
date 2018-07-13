e9784b18375801c5255f31a9a74af3fb131279f5
silky <noonsilk+-noonsilk@gmail.com>
2018-07-13 16:19:29 +1000

    works\?

--- src/Main.hs
+++ src/Main.hs
@@ -1,23 +1,59 @@
-{-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE CPP #-}
-{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeFamilies #-}
 
 module Main where
 
+import Prelude hiding (FilePath)
 import Control.Monad.IO.Class
+import Options.Applicative
+import Data.Monoid
+import Data.String.Conv
 import Data.Tagged
-import Git
-import Git.Tree.Working
+import Git hiding (Options)
 import Git.Libgit2 (MonadLg, LgRepo, lgFactory)
+import Git.Tree.Working
+import Shelly
+
+
+data Options = Options
+  { repoDir :: String
+  , destDir :: String
+  }
+
+
+options :: Parser Options
+options = Options
+  <$> strOption (short 'r' <> long "repo-dir" <> value "."
+                  <> help "The directory of the repository to flatten. Default: \".\"")
+  <*> strOption (short 'd' <> long "dest-dir"
+                  <> help "The output directory")
+
 
 main :: IO ()
-main = do
-  withRepository lgFactory ".." $ do
-    liftIO $ putStrLn "Opened repo!"
+main = execParser opts >>= go
+  where
+    opts = info (helper <*> options)
+                (fullDesc <> progDesc desc <> header hdr)
+    hdr  = "git-everything - flatten a git repository by commit."
+    desc = "Flatten a git repository by checking out all the commits into a particular folder."
 
+
+-- destDir, repoDir :: String
+-- destDir = "/home/noon/tmp/git-everything"
+-- repoDir = "."
+
+
+go :: Options -> IO ()
+go opts = do
+  withRepository lgFactory (repoDir opts) $ do
     Just ref <- resolveReference "HEAD"
-    flip traverseCommits_ (Tagged ref) $ \c -> do
-      liftIO $ putStrLn "Found Commit!"
+    flip traverseCommits_ (Tagged ref) $ \tc -> do
+      c <- lookupCommit tc
+
+      let (Tagged cid) = tc
+          newDir       = destDir opts ++ "/" ++ show cid     
+
+      liftIO $ shelly $ do
+        cmd "git" ["clone", repoDir opts, newDir]
+        cd  (fromText (toS newDir))
+        cmd "git" ["reset", "--hard", show cid]
+
